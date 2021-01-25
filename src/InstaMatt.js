efa04eb750e59a286f47b3a49a1de70b7a0c9fa6
Matt Serrato <mattserrato@Matts-MBP.fibertel.com.ar>
2021-01-25 13:06:35 -0300

    layour and like function

--- src/InstaMatt.js
+++ src/InstaMatt.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { Image, Text, View, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, Text, View, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
+import config from './config';
 
 
 const InstaMatt = () => {
+	const [screenWidth, setScreenWidth] = useState(null);
+	const [liked, setLiked] = useState(false)
+	useEffect(() => {
+		setScreenWidth(Dimensions.get('window').width * 1.1);
+	}, []);
+
+	likeToggle = () => {
+		setLiked(!liked);
+	}
+
+	const heartIconColor = liked ? 'rgb(252,61,57)' : null;
 	return (
 		<View style={styles.container}>
 			<View style={styles.tempNav}><Text>InstaMatt</Text></View>
@@ -12,13 +24,28 @@ const InstaMatt = () => {
 						style={styles.profilePicture}
 						source={{uri: 'https://i.pinimg.com/280x280_RS/ed/ec/78/edec78ac2d3e5ea21020e946c54ae227.jpg'}}
 						></Image>
-					<Text>Mili Pili</Text>
+					<Text style={{marginLeft: 10}} >MiliPiliTestUser</Text>
+					
 				</View>
+				<View style={{ marginBottom: 10,flexDirection: 'column',alignItems:'center'}}><Text style={{fontSize: 30}}>...</Text></View>
 			</View>
+			<TouchableOpacity 
+			activeOpacity={0.7}
+			onPress={()=> likeToggle()}>
 			<Image
-				style={styles.imageSize}
+				style={{width: screenWidth, height:400, resizeMode: 'cover'}}
 			 	source={{uri: 'https://www.collabary.com/wp-content/uploads/2017/06/Break-The-Instagram-%E2%80%93-How-To-Capture-And-Create-The-Perfect-Influencer-Photo.jpg'}}
 			/>
+			</TouchableOpacity>
+			<View style={styles.iconBar}>
+				<Image style={[styles.icon, { tintColor: heartIconColor}]} source={config.images.heartIcon} />
+				<Image style={styles.icon} source={config.images.messageIcon} />
+				<Image style={styles.icon} source={config.images.arrowIcon} />
+			</View>
+			<View style={styles.iconBar}>
+			<Image style={{height:20, width: 20, tintColor: heartIconColor}} source={config.images.blackHeartIcon} />
+				<Text>128 Likes</Text>
+			</View>
 		</View>
 	);
 }
@@ -28,13 +55,6 @@ const styles = StyleSheet.create({
     flex: 1,
     width: 100 + '%',
     height: 100 + '%',
-	},
-	userBar: {
-		width: 100 + '%',
-		height: 50,
-		backgroundColor: 'rgb(255, 255, 255)',
-		flexDirection: 'row',
-		alignItems: 'center'
 	},
 	tempNav: {
 		width: 100 + '%',
@@ -47,11 +67,34 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
-	imageSize: {
-		width: 100 + '%', height: 100
+
+	userBar: {
+		width: 100 + '%',
+		height: config.styleConstants.rowHeight,
+		backgroundColor: 'rgb(255, 255, 255)',
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingHorizontal: 10,
+		justifyContent: 'space-between'
+	},
+	iconBar: {
+		height: config.styleConstants.rowHeight,
+		alignSelf: 'stretch',
+		borderColor: 'rgb(233,233,233)',
+		borderTopWidth: StyleSheet.hairlineWidth,
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	icon: {
+		marginLeft: 5,
+		height: 35,
+		width: 35,
 	},
 	profilePicture: {
-		width: 40, height: 40
+		width: 40,
+		height: 40,
+		borderRadius: 20
 	}
 });
 
