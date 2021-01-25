a627d624e4c4ee7b49f546967267756388c25fbf
Matt Serrato <mattserrato@Matts-MBP.fibertel.com.ar>
2021-01-25 13:04:34 -0300

    layout and like function

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+export default {
+  images: {
+    heartIcon: require('../../assets/heart.png'),
+    blackHeartIcon: require('../../assets/blackHeart.png'),
+    arrowIcon: require('../../assets/arrow.png'),
+    messageIcon: require('../../assets/message.png'),
+  },
+  styleConstants: {
+    rowHeight: 50,
+  }
+}
